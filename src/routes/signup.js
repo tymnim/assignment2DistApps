e76b87515b7be2b7b5ea7cced2f66edbcbe4433e
Tim Nimets <tymnim@gmail.com>
2020-10-22 17:03:59 -0400

    got signup working

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -12,19 +12,18 @@ exports.signUp = {
   authNeeded: false,
   callback: async function signUp(req, res) {
     try {
-      const data = await retrieveDataFrom(req)
-      console.log(data, req.body)
+      const { username, password } = await retrieveDataFrom(req)
       const encrypredPassword = await makeHashOf(password, SALT_ROUNDS)
       const exists = await userExists(username, global.db)
 
       if (exists) {
-        res.status(200)
-        res.send({ message: "User was created successfully!" })
+        res.status(409)
+        res.send({ error: "Username is already taken." })
         res.end();
         return
       }
       const inserted = await global.db.collection("users").insertOne({
-        name: userName,
+        name: username,
         password: {
           saltRounds: SALT_ROUNDS,
           hash: encrypredPassword
